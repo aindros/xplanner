ebbc95139ea38b379e90c5ee045bc6dcfbe586ce
Alessandro Iezzi <aiezzi@alessandroiezzi.it>
2023-10-27 00:11:47 +0200

    Move Thymeleaf configuration from XML to Java

--- src/main/java/xplanner/config/XPlannerConfiguration.java
+++ src/main/java/xplanner/config/XPlannerConfiguration.java
@@ -24,10 +24,15 @@ import com.technoetic.xplanner.security.Authenticator;
 import com.technoetic.xplanner.security.AuthenticatorImpl;
 import org.flywaydb.core.Flyway;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.thymeleaf.spring3.SpringTemplateEngine;
+import org.thymeleaf.spring3.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring3.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
 
 @Configuration
 public class XPlannerConfiguration {
@@ -58,5 +63,44 @@ public class XPlannerConfiguration {
 		String password = properties.getProperty("hibernate.connection.password");
 
 		return Flyway.configure().dataSource(conncetionUrl, username, password).load();
+	}
+
+	private @Autowired ApplicationContext applicationContext;
+
+	@Bean
+	public SpringResourceTemplateResolver templateResolver(){
+		// SpringResourceTemplateResolver automatically integrates with Spring's own
+		// resource resolution infrastructure, which is highly recommended.
+		SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+		templateResolver.setApplicationContext(this.applicationContext);
+		templateResolver.setPrefix("/WEB-INF/templates/");
+		templateResolver.setSuffix(".html");
+		// HTML is the default value, added here for the sake of clarity.
+		templateResolver.setTemplateMode(TemplateMode.HTML);
+		// Template cache is true by default. Set to false if you want
+		// templates to be automatically updated when modified.
+		templateResolver.setCacheable(true);
+		return templateResolver;
+	}
+
+	@Bean
+	public SpringTemplateEngine templateEngine(){
+		// SpringTemplateEngine automatically applies SpringStandardDialect and
+		// enables Spring's own MessageSource message resolution mechanisms.
+		SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+		templateEngine.setTemplateResolver(templateResolver());
+
+		return templateEngine;
+	}
+
+	@Bean
+	public ThymeleafViewResolver viewResolver(){
+		ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+		viewResolver.setTemplateEngine(templateEngine());
+		// NOTE 'order' and 'viewNames' are optional
+		viewResolver.setOrder(1);
+		viewResolver.setViewNames(new String[] {"thymeleaf/*.html"});
+
+		return viewResolver;
 	}
 }
