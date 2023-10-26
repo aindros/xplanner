ea28e033d8f9e1cc5da47fcb95bf20ccfffe5543
Alessandro Iezzi <aiezzi@alessandroiezzi.it>
2023-10-26 19:53:49 +0200

    Add Repository

---
+++ src/main/java/xplanner/repository/Repository.java
@@ -0,0 +1,25 @@
+/*-
+ * XPlanner is a software to keep track of your working activities
+ * Copyright (C) 2021, 2023  Alessandro Iezzi <aiezzi AT alessandroiezzi PERIOD it>
+ *
+ * XPlanner is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * XPlanner is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with XPlanner.  If not, see <https://www.gnu.org/licenses/>.
+ * package xplanner.controller;
+ */
+
+package xplanner.repository;
+
+public interface Repository<T, K> {
+	T findById(K id);
+	T findById(K id, boolean cachable);
+}
