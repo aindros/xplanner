4ca7ea92dff8e81a1776c225854f5602574c4472
Alessandro Iezzi <aiezzi@alessandroiezzi.it>
2023-10-30 00:27:44 +0100

    Add ProjectRepository

---
+++ src/main/java/xplanner/repository/ProjectRepository.java
@@ -0,0 +1,28 @@
+/*-
+ * XPlanner is a software to keep track of your working activities
+ * Copyright (C) 2021, 2023  Alessandro Iezzi <aiezzi AT alessandroiezzi PERIOD it>
+ *
+ * XPlanner is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * XPlanner is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with XPlanner.  If not, see <https://www.gnu.org/licenses/>.
+ * package xplanner.controller;
+ */
+
+package xplanner.repository;
+
+import net.sf.xplanner.domain.Project;
+
+import java.util.List;
+
+public interface ProjectRepository extends Repository<Project, Integer> {
+	List<Project> findProjectsByHidden(boolean hidden);
+}
