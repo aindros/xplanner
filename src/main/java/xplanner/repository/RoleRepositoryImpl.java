9ec7b9bb15640100237dcf09d9b0a212ee832848
Alessandro Iezzi <aiezzi@alessandroiezzi.it>
2023-10-31 10:44:40 +0100

    Add src/main/java/xplanner/repository/RoleRepository.java

---
+++ src/main/java/xplanner/repository/RoleRepositoryImpl.java
@@ -0,0 +1,30 @@
+/*-
+ * XPlanner is a software to keep track of your working activities
+ * Copyright (C) 2021, 2023  Alessandro Iezzi <aiezzi AT alessandroiezzi PERIOD it>
+ *
+ * XPlanner is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * XPlanner is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with XPlanner.  If not, see <https://www.gnu.org/licenses/>.
+ * package xplanner.controller;
+ */
+
+package xplanner.repository;
+
+import net.sf.xplanner.domain.Role;
+import org.springframework.stereotype.Component;
+
+@Component("roleRepository2")
+public class RoleRepositoryImpl extends BaseRepository<Role, Integer> implements RoleRepository {
+	protected RoleRepositoryImpl() {
+		super(Role.class);
+	}
+}
