57e5f662f5028daf553934c91634389e580ba938
Alessandro Iezzi <aiezzi@alessandroiezzi.it>
2023-11-01 16:50:15 +0100

    Add parameters to scripts/update-version.sh

--- scripts/update-version.sh
+++ scripts/update-version.sh
@@ -4,14 +4,26 @@ PROPERTIES=src/main/resources/xplanner.properties
 VERSION_POM=`grep '<version' 'pom.xml' | head -1 | sed -E 's/.*<.*>(.*)<.*>/\1/g'`
 VERSION_PROP=`grep 'xplanner.version' $PROPERTIES | sed 's/.*=//g'`
 BUILD_DATE=`date +%d/%m/%Y`
+BUILD_REV=$(expr `grep 'xplanner.build.revision' $PROPERTIES | sed 's/.*=//g'` + 1)
 
-if [ "$VERSION_POM" != "$VERSION_PROP" ]; then
-    BUILD_REV=0
+if [ $# -gt 0 ]; then
+	case $1 in
+		--version)
+			UPDATE_VERSION=1
+			;;
+		--revision)
+			UPDATE_REVISION=1
+			;;
+	esac
 else
-    BUILD_REV=`grep 'xplanner.build.revision' $PROPERTIES | sed 's/.*=//g'`
-    BUILD_REV=`expr $BUILD_REV + 1`
+	UPDATE_VERSION=1
 fi
 
-sed -i '' -E "s/(xplanner.version=).*/\1${VERSION_POM}/g" $PROPERTIES
-sed -i '' -E "s|(xplanner.build.date=).*|\1${BUILD_DATE}|g" src/main/resources/xplanner.properties
-sed -i '' -E "s|(xplanner.build.revision=).*|\1${BUILD_REV}|g" src/main/resources/xplanner.properties
+if [ ! -z $UPDATE_VERSION ]; then
+	sed -i '' -E "s/(xplanner.version=).*/\1${VERSION_POM}/g" $PROPERTIES
+fi
+
+if [ ! -z $UPDATE_REVISION ]; then
+	sed -i '' -E "s|(xplanner.build.date=).*|\1${BUILD_DATE}|g" src/main/resources/xplanner.properties
+	sed -i '' -E "s|(xplanner.build.revision=).*|\1${BUILD_REV}|g" src/main/resources/xplanner.properties
+fi
