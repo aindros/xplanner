050843adbc492b5cb986b7730f422a9b17fe98ab
Alessandro Iezzi <aiezzi@alessandroiezzi.it>
2023-11-02 15:28:59 +0100

    Fix scripts/update-version.sh

--- scripts/update-version.sh
+++ scripts/update-version.sh
@@ -4,7 +4,7 @@ PROPERTIES=src/main/resources/xplanner.properties
 VERSION_POM=`grep '<version' 'pom.xml' | head -1 | sed -E 's/.*<.*>(.*)<.*>/\1/g'`
 VERSION_PROP=`grep 'xplanner.version' $PROPERTIES | sed 's/.*=//g'`
 BUILD_DATE=`date +%d/%m/%Y`
-BUILD_REV=$(expr `grep 'xplanner.build.revision' $PROPERTIES | sed 's/.*=//g'` + 1)
+BUILD_REV=$(expr `grep -E '^xplanner.build.revision' $PROPERTIES | sed 's/.*=//g'` + 1)
 
 if [ $# -gt 0 ]; then
 	case $1 in
@@ -30,5 +30,5 @@ fi
 
 if [ ! -z $UPDATE_REVISION ]; then
 	sed -i '' -E "s|(xplanner.build.date=).*|\1${BUILD_DATE}|g" src/main/resources/xplanner.properties
-	sed -i '' -E "s|(xplanner.build.revision=).*|\1${BUILD_REV}|g" src/main/resources/xplanner.properties
+	sed -i '' -E "s|^(xplanner.build.revision=).*|\1${BUILD_REV}|g" src/main/resources/xplanner.properties
 fi
